f8cfd4a4e924992143a230f7f625731bbdfcc8ca
Sumeet Bansal <sumeetbansal@gmail.com>
2017-08-22 14:55:37 -0700

    added basic button inject

--- app/inject.js
+++ app/inject.js
@@ -5,3 +5,10 @@
  * 4. generate Google Calendar
  * 5. populate calendar obj-by-obj
 */
+
+// inject export button
+var exportButton = $("#add-event-id").clone(true);
+exportButton.attr('id', "export-id");
+exportButton.attr('value', "Export");
+$("#schedule-addevent-div").append(exportButton);
+$("#export-id").copyCSS($("#add-event-id"));
