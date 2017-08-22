13c39d62023bb3cf6b86c09ee903ae7130f7ea57
Sumeet Bansal <sumeetbansal@gmail.com>
2017-08-22 10:29:39 -0700

    basic setup

---
+++ app/inject.js
@@ -0,0 +1,7 @@
+/* TODO
+ * 1. inject export button
+ * 2. isolate schedule from page DOM
+ * 3. process each row as class obj
+ * 4. generate Google Calendar
+ * 5. populate calendar obj-by-obj
+*/
